565bbf3cf91b436206d25e0443d65ce0c2cb962e
Matt Campbell <matt.campbell@vibes.com>
2020-12-18 15:40:01 -0600

    Render the board with loops

--- src/index.tsx
+++ src/index.tsx
@@ -35,21 +35,11 @@ class Board extends React.Component<BoardProps> {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[0, 1, 2].map((i) => (
+          <div key={i} className="board-row">
+            {[0, 1, 2].map((j) => this.renderSquare(i * 3 + j))}
+          </div>
+        ))}
       </div>
     );
   }
